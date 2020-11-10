ca9cede38b3a95147e61c6c8d32be93be5dbd7e0
Madhav Anand <madhavanand402@gmail.com>
2020-11-10 15:42:48 +0530

    Completed

---
+++ 1. Hello World/script.js
@@ -0,0 +1,9 @@
+alert("Hello Buddy");
+
+// This can't be loaded in "async" manner
+document.getElementById("h").innerHTML = "Madhav Anand";
+
+// This can't be loaded in "defer" manner
+document.write("Madhav Anand");
+
+console.log("Hello World");
