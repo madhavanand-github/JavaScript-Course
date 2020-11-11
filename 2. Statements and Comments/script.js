d7208369231a628a65aaeb121d336e7c262914b5
Madhav Anand <madhavanand402@gmail.com>
2020-11-11 15:35:21 +0530

    Lecture 2 Added

---
+++ 2. Statements and Comments/script.js
@@ -0,0 +1,17 @@
+// Method #1 
+    // Like English Paragraphs
+var a; a=1; alert(a);
+
+// Method #2
+    // This is just to remind you that statement ends with semi-colon
+var b;
+b
+=
+2;
+alert(b);
+
+// Method #3
+    //This is recommended.
+var c;
+c = 3;
+alert(c);
