4118875c763061c869d00da031759e0496bb1cec
Madhav Anand <madhavanand402@gmail.com>
2020-11-20 15:45:52 +0530

    Variable Added

--- 3. Variables/script.js
+++ 3. Variables/script.js
@@ -1,9 +1,13 @@
 
 // Variables are containers to store any data
+// Naming Convention: Starts only from $, _, {letter}
+// Case Sensitive: digit and Digit both are different
 
-var x = 5; // Old Method
-let y = 5; // New Method
-y = 10; // Value can be changed
+var x = 5; // Old Method to declare a variable
+let y = 5; // New Method to declare a variable
+y = 10; // ___IMP___ Value can be changed
+
+// Few Examples
 let m = 1, n = 2;
 let firstName = "Madhav";
 let lastName = 'Anand';
@@ -12,5 +16,4 @@ fullName = "Madhav Anand";
 
 const pie = 3.14; // Value can't be changed
 
-// Naming Convention: Starts only from $, _, {letter}
-// Case Sensitive: digit and Digit both are different
+
