6b99445c95e920f98db77b10723d3e5f8b453b49
Madhav Anand <madhavanand402@gmail.com>
2020-11-20 15:48:50 +0530

    Updated

--- 2. Statements and Comments/script.js
+++ 2. Statements and Comments/script.js
@@ -1,17 +1,14 @@
-// Method #1 
-    // Like English Paragraphs
-var a; a=1; alert(a);
+// #1 Like English Paragraphs
+var a; a=1; console.log(a);
 
-// Method #2
-    // This is just to remind you that statement ends with semi-colon
+// #2 Reminder: statement ends with semi-colon
 var b;
 b
 =
 2;
-alert(b);
+console.log(b);
 
-// Method #3
-    //This is recommended.
+// #3 Recommended.
 var c;
 c = 3;
-alert(c);
+console.log(c);
