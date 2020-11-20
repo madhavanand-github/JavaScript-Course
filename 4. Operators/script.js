233ec66578e8f5bb18350797fca3e90c87f067e7
Madhav Anand <madhavanand402@gmail.com>
2020-11-20 15:44:18 +0530

    Operators Added

---
+++ 4. Operators/script.js
@@ -0,0 +1,35 @@
+
+// '=' Assignment Operator
+let x = 5, y = 5;
+
+// '==' Equal To Operator: "True/False"
+let z = (x==y);
+console.log(z);
+
+// Basic Operators: +,-,*,/,%
+
+// Short Assignment
+x+=5;
+console.log(x);
+
+// Exponent
+x = x**2;
+console.log(x);
+
+// Concatenation
+console.log(x + " " + '5');
+
+// Increment ___IMP___
+console.log(x++);
+console.log(x--);
+console.log(x);
+
+// Increment ___IMP___
+y++;
+console.log(y);
+y--;
+console.log(y);
+
+// Maths equation works in same way
+let ans = y + 5/5;
+console.log(ans);
