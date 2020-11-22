3b32964eae8c7b01b5109d471f7b326c51cc5ef2
Madhav Anand <madhavanand402@gmail.com>
2020-11-22 21:40:16 +0530

    Conditions Added

---
+++ 7. Condition/script.js
@@ -0,0 +1,16 @@
+// Ternary Operator: Conditional Assignment
+let isloggerin = 1;
+let answer = isloggerin ? "Logged In" : "Logged Out" ;
+console.log(answer);
+
+// Nullish Coalescing Operator
+let user1 = null;
+console.log(user1 ?? "Guest User 1");
+let user2;
+console.log(user2 ?? "Guest User 2");
+
+// if Condition
+// if-else condition  
+// if-else-ladder condition
+// if-else-nested condition
+
