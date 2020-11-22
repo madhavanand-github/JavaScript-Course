d544275380c1c7bb4135ebdc2ee2de3e0316faaf
Madhav Anand <madhavanand402@gmail.com>
2020-11-22 21:33:13 +0530

    Comparison Operators Added

---
+++ 6. Logical & Comparison Operator/script.js
@@ -0,0 +1,28 @@
+// COMPARISON OPERATORS: Check if 'true' or 'false'
+
+// '==' equal to 'value'
+let a = (5=='5');
+console.log(a);
+
+// '===' equal to 'type' and then 'value'
+let b = (5==='5');
+console.log(b);
+
+// '!=' not equal to 'value'
+let c = (5 != '5');
+console.log(c);
+
+// '!=' not equal to 'type' and then 'value'
+let d = (5 !== '5');
+console.log(d);
+
+// '>' Greater than
+// '>=' Greater than equal to
+// '<'  Less than
+// '<=' Less than equal to
+
+// LOGICAL OPERATORS: Compares multiple 'true'/'false' values
+
+// '&&' AND: True if all conditions are true
+// '||' OR: True if any one condition is true
+// '!' NOT: Reverse the value
